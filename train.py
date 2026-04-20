f70d1c23a2e4c9a6afd54ff4ed52ee39ccf34e3b
David-Gbenga <olanipekunkolawole90@gmail.com>
2026-04-20 17:02:35 +0100

    train.py file

---
+++ train.py
@@ -0,0 +1,59 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import joblib
+import os
+import json
+
+
+def train_model():
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y)
+    
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_train, y_train)
+    
+    
+    
+    #save model 
+    os.makedirs("artifacts", exist_ok =True)
+    model_path = os.path.join("artifacts", "model.pkl")
+    joblib.dump(model, model_path)
+    
+    #save metrics
+    y_pred = model.predict(X_test)
+    metrics = accuracy_score(y_test, y_pred)  
+    with open(os.path.join("artifacts", "metrics.json"), "w") as f:
+        json.dump({"accuracy": metrics}, f) 
+        
+    print(f"Saved model to {model_path}")
+    print(f"Test accuracy: {metrics:.4f}")
+    
+if __name__ == "__main__":
+    train_model()    
+    
+
+    
+    
+    
+    
+
+    
+
+
+    
+    
+    
+
+    
+    
+    
+    
+
+    
+    
+    
+
